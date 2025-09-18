a25e4f9c070e67ade716d85f32f3fe02fff43074
Sakshi Shukla <sakshishu09@gmail.com>
2025-09-18 23:50:17 +0530

    Time: 1 ms (84.27%), Space: 40.8 MB (70.34%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int mySqrt(int x) {
+       if (x<2)
+       return x;
+       int low=1, high = x/2; 
+       int ans =0;
+    //    kyunki square root x/2 se zyada kabhi nahi hoga except x=1,2).
+     
+    while(low<=high){
+        int mid = low+(high-low)/2;
+        long sq = (long) mid * mid;
+        if (sq== x){
+            return mid;
+        }
+        else if (sq <x){
+            ans = mid;
+            low = mid+1;
+        }
+        else{
+            high = mid-1;
+        }
+    }
+    return ans;
+    }
+}
