c047942881e97b0501dff5a094a39c2b98ec22e7
Sakshi Shukla <sakshishu09@gmail.com>
2025-09-29 22:24:55 +0530

    Time: 0 ms (100%), Space: 46.9 MB (14.59%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int trap(int[] height) {
+        int l = 0;
+        int r = height.length-1;
+        int lmax = 0,rmax =0;
+        int ans =0;
+        while(l<=r){
+            if (height[l]<=height[r]){
+            if (height[l]>=lmax){  
+              lmax = height[l];
+            }
+            else {
+               ans += lmax - height[l];
+            }
+            l++;
+            }
+            else {
+                 if (height[r] >= rmax) {
+                    rmax= height[r];
+                } else {
+                    ans += rmax - height[r];
+                }
+                r--;
+            }
+        }
+        return ans;
+
+    }
+}
