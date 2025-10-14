12ced46dc7752f7b8025ed3493517a32c6920426
Sakshi Shukla <sakshishu09@gmail.com>
2025-10-14 17:32:41 +0530

    Time: 1 ms (97.61%), Space: 43 MB (79.63%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,26 +1,32 @@
-class Solution {
+
+       class Solution {
     public int thirdMax(int[] nums) {
-         long max1=Long.MIN_VALUE;
-        long max2=Long.MIN_VALUE;
-        long max3=Long.MIN_VALUE;
-        HashSet<Integer> set=new HashSet<>();
-        for(int i:nums){
-            if (i == max1 || i == max2 || i == max3) continue;
-            set.add(i);
-            if(i>max1 ){
-                max3=max2;
-                max2=max1;
-                max1=i;
+        return solve(nums);
+    }
+    private int solve(int[] nums)
+    {
+        long mx1 = Long.MIN_VALUE;
+        long mx2 = Long.MIN_VALUE;
+        long mx3= Long.MIN_VALUE;
+        for(int num: nums){
+            if(num>mx1){
+                mx3=mx2;
+                mx2=mx1;
+                mx1=num;
             }
-            else if(i>max2){
-                max3=max2;
-                max2=i;
+            else if(num>mx2 && num!=mx1){
+                mx3=mx2;
+                mx2=num;
             }
-            else if(i>max3){
-                max3=i;
+            else if(num>mx3 && num!=mx1 && num!=mx2){
+                mx3=num;
             }
         }
-       return (max3==Long.MIN_VALUE)?(int)max1:(int)max3;
-        
+        if(mx3 != Long.MIN_VALUE){
+            return (int)mx3;
+        }
+        return (int)mx1;
     }
 }
+        
+   
