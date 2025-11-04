8ec08414e4bc6c40330cb6aa87f62c5691e709ed
Sakshi Shukla <sakshishu09@gmail.com>
2025-11-04 19:13:25 +0530

    Time: 49 ms (10.39%), Space: 90.8 MB (5.08%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.java
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maximumGap(int[] nums) {
-        Arrays.sort(nums);
+        Arrays.parallelSort(nums);
        long mx = -1L;
        long diff = 0L;
         for (int i = 1; i<nums.length;i++){
@@ -8,10 +8,7 @@ class Solution {
           diff = Math.abs((long)nums[i] - (long)nums[i-1]);
               mx = Math.max(mx,diff);
            }
-        
         }
-       
-        
         if (nums.length<2) 
         return 0;
        if (mx == -1L)
