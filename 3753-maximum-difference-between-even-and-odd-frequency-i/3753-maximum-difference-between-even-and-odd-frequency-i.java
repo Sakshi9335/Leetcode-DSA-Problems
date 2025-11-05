559aa278af5a49fec349b40e7491fc57c3a66e89
Sakshi Shukla <sakshishu09@gmail.com>
2025-11-05 10:58:31 +0530

    Time: 1 ms (100%), Space: 43.7 MB (8.6%) - LeetHub

--- 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.java
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.java
@@ -1,26 +1,21 @@
 class Solution {
     public int maxDifference(String s) {
     
-        HashMap<Character,Integer> map =new HashMap<>();
-         int maxOdd = Integer.MIN_VALUE;
-        int minEven = Integer.MAX_VALUE;
-        boolean hasOdd = false, hasEven = false;
+         int[] arr = new int[26];
+        for(char ch : s.toCharArray()) {
+            arr[ch-'a']++;
+        }
 
-        for (char ch : s.toCharArray()){
-            map.put(ch,map.getOrDefault(ch,0)+1);
-        }
-             for(int freq :map.values()) {  
-            if(freq % 2 == 0){
-                 hasEven = true;
-              minEven = Math.min(minEven, freq);
-            }
-            else {
-                 hasOdd = true;
-                 maxOdd = Math.max(maxOdd, freq);
+        int oddFreq = 0, evenFreq = Integer.MAX_VALUE;
+
+        for(int i : arr) {
+            if(i != 0 && i % 2 == 0) {
+                evenFreq = Math.min(evenFreq, i);
+            } else {
+                oddFreq = Math.max(oddFreq, i);
             }
         }
-       if (!hasOdd) return -minEven;
-        if (!hasEven) return maxOdd;
-        return maxOdd - minEven;
+
+        return oddFreq - evenFreq;
     }
 }
