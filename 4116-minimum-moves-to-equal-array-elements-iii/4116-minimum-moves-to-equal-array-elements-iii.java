9af3f4c7efb1a720cd330f0582bb18d043f022e4
Sakshi Shukla <sakshishu09@gmail.com>
2025-11-08 21:55:55 +0530

    Time: 1 ms (100%), Space: 46.3 MB (0%) - LeetHub

--- 4116-minimum-moves-to-equal-array-elements-iii/4116-minimum-moves-to-equal-array-elements-iii.java
+++ 4116-minimum-moves-to-equal-array-elements-iii/4116-minimum-moves-to-equal-array-elements-iii.java
@@ -1,18 +1,27 @@
 class Solution {
     public int minMoves(int[] nums) {
-        int largest = Integer.MIN_VALUE;
+        // int largest = Integer.MIN_VALUE;
+        //     int n = nums.length;
+        //     for (int i =0; i<n;i++){
+        //     if (nums[i]>largest){
+        //         largest = nums[i];
+        //     }
+        //     }
+        // int count =0;
+        // for (int i =0; i<n;i++){
+        //     if (nums[i]<largest){
+        //         count += (largest - nums[i]);
+        //     }
+        // }
+
+       int largest = Integer.MIN_VALUE;
             int n = nums.length;
+            int sum =0;
             for (int i =0; i<n;i++){
-            if (nums[i]>largest){
-                largest = nums[i];
+             largest =   Math.max(largest,nums[i]);
+             sum = sum+nums[i];
             }
-            }
-        int count =0;
-        for (int i =0; i<n;i++){
-            if (nums[i]<largest){
-                count += (largest - nums[i]);
-            }
-        }
-        return count;
+
+        return (largest*n - sum);
     }
 }
