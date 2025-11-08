bdb1c27eb0f82f8331c7ce1c7e97a48ac6c6b03f
Sakshi Shukla <sakshishu09@gmail.com>
2025-11-08 20:02:46 +0530

    Time: 39 ms (75.78%), Space: 98.8 MB (5.15%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        int n = nums.length;
+     Stack <Integer> st = new Stack<>();
+     int k = Integer.MIN_VALUE;
+
+     for(int i = n-1; i>=0; i--){
+          if (nums[i] < k)
+        return true;
+
+    while (!st.isEmpty() && nums[i] > st.peek()) {
+        k = st.pop();
+    }
+
+    st.push(nums[i]);
+     }
+        return false;
+    }
+}
