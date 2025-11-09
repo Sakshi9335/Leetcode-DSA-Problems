a22916079d9c04325b3d6dd8ef402bd914595efe
Sakshi Shukla <sakshishu09@gmail.com>
2025-11-09 19:11:28 +0530

    Time: 301 ms (19%), Space: 67 MB (24.91%) - LeetHub

---
+++ 3071-drop-duplicate-rows/3071-drop-duplicate-rows.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
+      customers = customers.drop_duplicates(subset=["email"])
+
+      return customers
+
+    
+    
