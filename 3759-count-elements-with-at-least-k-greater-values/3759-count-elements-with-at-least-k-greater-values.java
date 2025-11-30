5274bebded5c4c00f3e0293061ae0b4bdd241807
Sakshi Shukla <sakshishu09@gmail.com>
2025-11-30 10:34:17 +0530

    Time: 43 ms (100%), Space: 134.2 MB (0%) - LeetHub

--- 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.java
+++ 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.java
@@ -2,30 +2,24 @@ class Solution {
     public int countElements(int[] nums, int k) {
         int n = nums.length;
         Arrays.sort(nums);
+        if (k == 0) 
+        return n;
 
         int count = 0;
+        int i = 0;
 
-        for (int i = 0; i < n; i++) { 
-            int idx = upperBound(nums, nums[i]);
+        while (i < n) {
+            int value = nums[i];
+            int start = i;
+            while (i < n && nums[i] == value) i++;
 
-            int greater = n - idx;
+            int freq = i - start;
+            int greater = n - i; 
 
             if (greater >= k)
-                count++;
+                count += freq;
         }
 
         return count;
-    }
-
-    private int upperBound(int[] nums, int target) {
-        int low = 0, high = nums.length;
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] <= target)
-                low = mid + 1;
-            else
-                high = mid;
-        }
-        return low;
     }
 }
