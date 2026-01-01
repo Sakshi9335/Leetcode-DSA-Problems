cf2378999aca81dbb7f30ed560829b56f8f8aa8b
Sakshi Shukla <sakshishu09@gmail.com>
2026-01-01 09:57:48 +0530

    Time: 0 ms (100%), Space: 43.7 MB (7.44%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,15 +1,16 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        for (int i = digits.length - 1; i >= 0; i--) {
-            if (digits[i] != 9) {
+        int n = digits.length;
+        for(int i=n-1; i>=0; i--){
+            if(digits[i] != 9){
                 digits[i]++;
                 return digits;
-            } else {
-                digits[i] = 0;
             }
+            else 
+            digits[i]=0;
         }
-        int[] res = new int[digits.length + 1];
-        res[0] = 1;
+        int [] res =new int [n+1];
+        res[0] =1;
         return res;
     }
 }
