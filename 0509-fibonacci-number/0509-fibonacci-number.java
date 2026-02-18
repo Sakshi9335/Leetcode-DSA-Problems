d966201cd133b0d3f832fffd78e684f118cd2827
Sakshi Shukla <sakshishu09@gmail.com>
2026-02-18 10:04:06 +0530

    Time: 41 ms (9.53%), Space: 46.2 MB (14.31%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,7 +1,19 @@
 class Solution {
     public int fib(int n) {
-        if (n == 0 || n== 1)
-        return n;
-     return (fib(n-1)+fib(n-2));
+       int dp[]=new int[n+1];
+       Arrays.fill(dp,-1);
+       return solve(n,dp);
+
+    }
+
+    public int solve(int n,int[] dp){
+         if (n == 0 || n== 1)
+        return dp[n]=n;
+
+      if(dp[n]!=-1)
+      return dp[n];
+
+        
+     return dp[n]=fib(n-1)+fib(n-2);
     }
 }
