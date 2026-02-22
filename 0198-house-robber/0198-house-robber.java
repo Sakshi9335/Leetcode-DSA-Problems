3165cc804aee0ef6d0a029c114aaad38293e572c
Sakshi Shukla <sakshishu09@gmail.com>
2026-02-22 21:20:08 +0530

    Time: 0 ms (100%), Space: 42.9 MB (56.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -3,20 +3,18 @@ class Solution {
         int n=nums.length;
         int[] dp = new int [n+1];
         Arrays.fill(dp,-1);
-        return solve(n-1,dp,nums);
+        return solve(0,dp,nums);
     }
     public int solve(int i, int[]dp,int[]nums){
-        if(i==-1){
+         if (i >= nums.length)
+        {
             return 0;
-        }
-        if(i==0){
-            return nums[i];
         }
         if(dp[i]!=-1){
             return dp[i];
         }
-        int pick = nums[i]+solve(i-2,dp,nums);
-        int nonpick = solve(i-1,dp,nums);
+        int pick = nums[i]+solve(i+2,dp,nums);
+        int nonpick = solve(i+1,dp,nums);
         return dp[i] = Math.max(pick,nonpick);
     }
 }
