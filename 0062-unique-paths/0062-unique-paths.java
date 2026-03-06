4973f9af68ba6465fad8ff894213bbd597a46c8e
Sakshi Shukla <sakshishu09@gmail.com>
2026-03-06 14:47:03 +0530

    Time: 0 ms (100%), Space: 42.1 MB (82.09%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,22 +1,28 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] grid = new int[m][n];
-        int[][] dp = new int[m + 1][n + 1];
-        for (int i = 0; i < dp.length; i++) 
+
+        int[][] dp = new int[m][n];
+
+        for (int i = 0; i < m; i++)
             Arrays.fill(dp[i], -1);
-        return solve(0, 0, dp, grid, m, n);
+
+        return solve(0, 0, dp, m, n);
     }
 
-    public int solve(int i, int j, int[][] dp, int[][] grid, int m, int n) {
-        if (i == m - 1 && j == n - 1) 
+    public int solve(int i, int j, int[][] dp, int m, int n) {
+
+        if (i == m - 1 && j == n - 1)
             return 1;
-        if (i < 0 || j < 0 || i >= m || j >= n) 
-            return 0;   
-        if (dp[i][j] != -1) 
+
+      if(i < 0 || j < 0 || i >= m || j >= n)
+            return 0;
+
+        if (dp[i][j] != -1)
             return dp[i][j];
-        
-        int right = solve(i + 1, j, dp, grid, m, n);
-        int down = solve(i, j + 1, dp, grid, m, n);
+
+        int right = solve(i + 1, j, dp, m, n);
+        int down = solve(i, j + 1, dp, m, n);
+
         return dp[i][j] = right + down;
     }
 }
