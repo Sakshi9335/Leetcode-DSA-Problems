a546a18437dd9c3a49680bde8968acc0ab718263
Sakshi Shukla <sakshishu09@gmail.com>
2026-03-08 09:35:07 +0530

    Time: 0 ms (100%), Space: 44.9 MB (6.14%) - LeetHub

---
+++ 3861-minimum-capacity-box/3861-minimum-capacity-box.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minimumIndex(int[] capacity, int itemSize) {
+        int ans =-1;
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i<capacity.length; i++){
+            if(capacity[i]>=itemSize  && capacity[i]<min )
+            {
+               min =capacity[i];
+                ans=i;
+            }
+        }
+        return ans;
+    }
+}
