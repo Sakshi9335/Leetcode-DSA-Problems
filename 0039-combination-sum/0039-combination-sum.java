8da676d55a845d093b7786e55caa5309dde636d6
Sakshi Shukla <sakshishu09@gmail.com>
2026-08-08 15:23:29 +0530

    Time: 3 ms (33.78%), Space: 45.9 MB (33.7%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,5 +1,4 @@
 class Solution {
-       Set<List<Integer>> set = new HashSet<>();
     List<List<Integer>> ans=new ArrayList<>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
        List<Integer> list = new ArrayList<>();
@@ -9,17 +8,12 @@ class Solution {
     public void helper(List<Integer>list,int[]arr,int i,int tar){
              if(i==arr.length || tar<0)  return;
                 if(tar==0){
-                    if(!set.contains(list))
                     ans.add(new ArrayList<>(list));
-                     set.add(new ArrayList<>(list));
                 return;
                 }
 
         list.add(arr[i]);
-         helper(list,arr,i+1,tar-arr[i]);    // include single element 
-
-         helper(list,arr,i,tar-arr[i]);    // include multiple element 
-
+         helper(list,arr,i,tar-arr[i]);    // include 
          list.remove(list.size()-1);   //backtrak
           helper(list,arr,i+1,tar);   //exclude
   
