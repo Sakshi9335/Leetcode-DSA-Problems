f3628b4cb7255cd57234411a4540feb292dfe551
Sakshi Shukla <sakshishu09@gmail.com>
2026-08-21 21:42:58 +0530

    Time: 8 ms (92.75%), Space: 46.1 MB (46.51%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if (n<=0){
-            return false;
-        }
-        while(n % 3 == 0){
-            n = n / 3;
-        }
-        return n==1;
+        if(n<=0)
+         return false;
+        else if(n==1)
+         return true;
+        else if(n%3==0)
+        return isPowerOfThree(n/3);
+    return false;
     }
 }
