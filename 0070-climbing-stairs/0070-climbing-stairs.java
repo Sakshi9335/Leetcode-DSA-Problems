ed3a1c7faa6fa1de9826ac973e0185fc086f4ec0
Sakshi Shukla <sakshishu09@gmail.com>
2026-09-06 13:56:55 +0530

    Time: 0 ms (100%), Space: 41.5 MB (99.1%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,17 @@
 class Solution {
     public int climbStairs(int n) {
-        int[]dp = new int [n+1];
+        int[]dp=new int[n+1];
         Arrays.fill(dp,-1);
-        return solve(n,dp);
+        return solve(dp,n);
+
     }
-    public int solve(int n, int[]dp){
-        if(n==1 || n==2){
+    public int solve(int[]dp,int n){
+        if(n<=2){
             return dp[n]=n;
         }
-        if(dp[n]!=-1){
+        if(dp[n] != -1){
             return dp[n];
         }
-        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+        return dp[n]=solve(dp,n-1)+solve(dp,n-2);
     }
-    
 }
